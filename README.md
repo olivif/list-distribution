a4497541cb610342c3f347287eed9b7d02ef34b6
olivif <olly2204@yahoo.com>
2015-12-21 23:46:20 +0100

    Adding list-distribution functionality. Adding tests. Adding travis + codecov.

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # list-distribution
 A helper package for analyzing the distribution of a list based on a field in every item.
+
+[![Build Status](https://travis-ci.org/olivif/list-distribution.svg?branch=master)](https://travis-ci.org/olivif/list-distribution)
+[![Coverage Status](https://codecov.io/github/olivif/list-distribution/coverage.svg?precision=2)](https://codecov.io/github/olivif/list-distribution)
+
+## How to use it
+
+Construct the distribution with the following parameters
+
+`items` - this is the list of items you want to operate on. it should be an array, and not be null, undefined or empty.
+ 
+`getter` - this is the getter function for the "key" that will be used to compute the distribution
+
+`precision` - this is the precision you want to have on the key, for instance if you use 100, all buckets will be in 100 intervals
+ 
+```js
+var distribution = new Distribution(items, getter, precision);
+distribution.printBuckets();
+```
+
+## An actual example
+
+Let's say you have a list of git repositories, and you want to see what the distribution of forks is over your list.  
+
+```js
+    var repos: [
+        { forks: 10,    watchers: 20    },
+        { forks: 14,    watchers: 243   },
+        { forks: 14523, watchers: 13442 },
+        { forks: 143,   watchers: 4232  },
+        { forks: 1555,  watchers: 322   },
+        { forks: 1,     watchers: 654   },
+        { forks: 103,   watchers: 54    }
+    ];
+```
+Your getter should be a function that takes in a repo object and selects the interest property off the repo object, in this case the forks count.
+
+```js
+var repoForksGetter = function(repo) { 
+    return repo.forks; 
+};
+```
+
+Now let's construct our distribution object, with a `precision` of 1000.
+
+```js
+var distribution = new Distribution(repos, repoForksGetter, 1000);
+```
+
+And now let's print it to see what's going on
+
+```js
+distribution.printBuckets();
+```
+
+This is what you should see 
+
+```sh
+Bucket 1000 -> 5
+Bucket 2000 -> 1
+Bucket 15000 -> 1
+```
+
+This is saying there are 5 repositories with less than 1000 forks, 1 repository with 1000-2000 forks and 1 repository with greater than 15000 forks.
+
+## Contributing
+
+For any ideas, suggestions for improvement or bugs, feel free to [file issues](https://github.com/olivif/list-distribution/issues)! PRs are more than welcome also! Thanks :tada: 
+     
---
+++ test/test.js
@@ -0,0 +1,248 @@
+const should = require("should");
+const stdout = require("test-console").stdout;
+
+const Distribution = require("./../lib/index");
+
+var TestData = {
+    Repos: [
+        { forks: 10,    watchers: 20    },
+        { forks: 14,    watchers: 243   },
+        { forks: 14523, watchers: 13442 },
+        { forks: 143,   watchers: 4232  },
+        { forks: 1555,  watchers: 322   },
+        { forks: 1,     watchers: 654   },
+        { forks: 103,   watchers: 54    }
+    ], 
+    RepoForksGetter: function(repo) { return repo.forks; },
+    RepoWatchersGetter: function(repo) { return repo.watchers; },
+    NoPrecision: 1,
+    Precision100: 100,
+    Precision1000: 1000,
+    Precision10000: 10000,
+    PrecisionMillion: 1000000,
+}
+
+describe("Distribution tests", function() {
+
+    describe("Distribution construction", function() {
+  
+        it("should be able to contruct a distribution object", function(done) {
+            
+            var distribution = new Distribution(TestData.Repos, TestData.RepoForksGetter, TestData.NoPrecision);
+            
+            should.exist(distribution);
+            
+            done();
+        });
+        
+        it("should not be able to contruct a distribution object with null or undefined items", function(done) {
+            
+            should(function() { var distrib = new Distribution(null, null, 0); })
+                .throw("Items is null or undefined");
+            
+            should(function() { var distrib = new Distribution(undefined, null, 0); })
+                .throw("Items is null or undefined");
+            
+            done();
+        });
+        
+        it("should not be able to contruct a distribution object with items which arent arrays", function(done) {
+            
+            should(function() { var distrib = new Distribution({}, null, 0); })
+                .throw("Items is not an array");
+            
+            done();
+        });
+        
+        it("should not be able to contruct a distribution object with items which is empty array", function(done) {
+            
+            should(function() { var distrib = new Distribution([], null, 0); })
+                .throw("Items is empty, no distribution be analyze");
+            
+            done();
+        });
+        
+        it("should not be able to contruct a distribution object with an null or undefined getter", function(done) {
+            
+            should(function() { var distrib = new Distribution(TestData.Repos, null, 0); })
+                .throw("Getter is null or undefined");
+            
+            should(function() { var distrib = new Distribution(TestData.Repos, undefined, 0); })
+                .throw("Getter is null or undefined");
+                
+            done();
+        });
+        
+        it("should not be able to contruct a distribution object with a getter which isnt a function", function(done) {
+            
+            should(function() { var distrib = new Distribution(TestData.Repos, 10, 0); })
+                .throw("Getter is not a function");
+            
+            done();
+        });
+        
+        it("should not be able to contruct a distribution object with a NaN precision", function(done) {
+            
+            should(function() { var distrib = new Distribution(TestData.Repos, TestData.RepoForksGetter, {}); })
+                .throw("Precision is not a number");
+            
+            done();
+        });
+        
+        
+        it("should not be able to contruct a distribution object with a precision < 1", function(done) {
+            
+            should(function() { var distrib = new Distribution(TestData.Repos, TestData.RepoForksGetter, 0); })
+                .throw("Precision should be >= 1");
+            
+            should(function() { var distrib = new Distribution(TestData.Repos, TestData.RepoForksGetter, -10); })
+                .throw("Precision should be >= 1");
+            
+            done();
+        });
+        
+        it("should not be able to contruct a distribution object with a double precision", function(done) {
+            
+            should(function() { var distrib = new Distribution(TestData.Repos, TestData.RepoForksGetter, 10.2); })
+                .throw("Precision is not a whole number");
+            
+            done();
+        });
+    });
+    
+    describe("Buckets", function() {
+
+        it("should be able to get buckets", function(done) {
+            
+            var distribution = new Distribution(TestData.Repos, TestData.RepoForksGetter, TestData.NoPrecision);
+            
+            should.exist(distribution);
+            
+            var buckets = distribution.getBuckets();
+            should.exist(buckets);
+            
+            done();
+        });
+        
+        
+        it("should be able to get initialize buckets with no precision", function(done) {
+            
+            var distribution = new Distribution(TestData.Repos, TestData.RepoForksGetter, TestData.NoPrecision);
+            
+            var buckets = distribution.getBuckets();
+            
+            should.exist(buckets);
+            
+            var keys = buckets.keys();
+            keys.length.should.eql(7);
+            
+            var values = buckets.values(); 
+            values.length.should.eql(7);
+            values.forEach(function(value) { value.should.eql(1); });
+            
+            done();
+        });
+        
+        it("should be able to get initialize buckets with precision 100", function(done) {
+            
+            var distribution = new Distribution(TestData.Repos, TestData.RepoForksGetter, TestData.Precision100);
+            distribution.printBuckets();
+            
+            var buckets = distribution.getBuckets();
+            
+            should.exist(buckets);
+            
+            var keys = buckets.keys();
+            keys.length.should.eql(4);
+            
+            var values = buckets.values(); 
+            values.length.should.eql(4);
+            
+            // Bucket 0 -> 3
+            // Bucket 100 -> 2
+            // Bucket 1500 -> 1
+            // Bucket 14500 -> 1
+            buckets.get(0).should.eql(3);
+            buckets.get(100).should.eql(2);
+            buckets.get(1500).should.eql(1);
+            buckets.get(14500).should.eql(1);
+            
+            done();
+        });
+        
+        it("should be able to get initialize buckets with precision 10000", function(done) {
+            
+            var distribution = new Distribution(TestData.Repos, TestData.RepoForksGetter, TestData.Precision10000);
+            distribution.printBuckets();
+            
+            var buckets = distribution.getBuckets();
+            
+            should.exist(buckets);
+            
+            var keys = buckets.keys();
+            keys.length.should.eql(2);
+            
+            var values = buckets.values(); 
+            values.length.should.eql(2);
+            
+            // Bucket 0 -> 6
+            // Bucket 10000 -> 1
+            buckets.get(0).should.eql(6);
+            buckets.get(10000).should.eql(1);
+            
+            done();
+        });
+        
+        it("should be able to get initialize buckets with precision million", function(done) {
+            
+            var distribution = new Distribution(TestData.Repos, TestData.RepoForksGetter, TestData.PrecisionMillion);
+            distribution.printBuckets();
+            
+            var buckets = distribution.getBuckets();
+            
+            should.exist(buckets);
+            
+            var keys = buckets.keys();
+            keys.length.should.eql(1);
+            
+            var values = buckets.values(); 
+            values.length.should.eql(1);
+            
+            buckets.get(0).should.eql(TestData.Repos.length);
+            
+            done();
+        });
+    });
+    
+    describe("Printing", function() {
+
+        it("should be able to print buckets", function(done) {
+            
+            var distribution = new Distribution(TestData.Repos, TestData.RepoForksGetter, TestData.NoPrecision);
+            
+            var output = stdout.inspectSync(function() {
+                distribution.printBuckets();
+            });
+
+            output.should.eql([
+                "Bucket 1 -> 1\n",
+                "Bucket 10 -> 1\n",
+                "Bucket 14 -> 1\n",
+                "Bucket 103 -> 1\n",
+                "Bucket 143 -> 1\n",
+                "Bucket 1555 -> 1\n",
+                "Bucket 14523 -> 1\n"
+            ]);
+
+            done();
+        });
+        
+        it("should be able to print example buckets", function(done) {
+
+            var distribution = new Distribution(TestData.Repos, TestData.RepoForksGetter, TestData.Precision1000);
+            distribution.printBuckets();
+            
+            done();
+        });
+    });
+});
